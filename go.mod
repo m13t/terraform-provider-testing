4e139bee2ae9aee63b25fe6a5b3ac8502eb3c432
Lewis Marsden-Lambert <lewis.lambert@zserve.co.uk>
2020-10-21 15:31:51 +0100

    Updating to SDK version 2

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/m13t/terraform-provider-testing
 
 go 1.14
 
-require github.com/hashicorp/terraform-plugin-sdk v1.10.0
+require (
+	github.com/hashicorp/terraform-plugin-sdk/v2 v2.0.4
+)
--- testing/factory.go
+++ testing/factory.go
@@ -1,6 +1,6 @@
 package testing
 
-import "github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+import "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
 func assertionFactory(t schema.ValueType) *schema.Schema {
 	return &schema.Schema{
